a4867d8acfa72d98fcf128d7b9ba2c3e3302cd4c
Sriva29 <63369374+Sriva29@users.noreply.github.com>
2024-03-25 17:20:34 -0400

    learned variables

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -3,4 +3,25 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+def main():
+    print("hello, world. . .")
+    name = input("Tu t'appelles quoi? ")
+    print("Enchante,", name)
+    if name == "Sri":
+        sub()
 
+def sub():
+    print("Sri the best")
+
+
+# sub()
+
+# if __name__ == "__main__":
+#     main()
+
+# print("hello, world. . .")
+# name = input("Tu t'appelles quoi? ")
+# print("Enchante,", name)
+# if name == "Sri":
+#     sub()
+# print(__name__)
